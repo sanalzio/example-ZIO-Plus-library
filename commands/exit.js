5ac512c3cf92514acafc4acb74d71688f02b3da8
Sanalzio <73523745+sanalzio@users.noreply.github.com>
2023-11-05 07:34:24 +0300

    Add files via upload

--- commands/exit.js
+++ commands/exit.js
@@ -1,6 +1,6 @@
 exports.name = "exit";
 exports.desc = "exit program";
-exports.handler = (cmd, arg, line, lines, editLine) => {
+exports.handler = (cmd, arg, line, lines, editLine, definitions, points) => {
     // Codes
     process.exit(arg.length > 1 ? Number(arg[1]) : 0)
 };
--- keys/pass.js
+++ keys/pass.js
@@ -1,5 +1,5 @@
 exports.name = "pass";
 exports.desc = "description";
-exports.handler = (cmd, arg, line, lines, editLine) => {
+exports.handler = (cmd, arg, line, lines, editLine, definitions, points) => {
     return;
 };
